213cea7b8a5921f6d084d8b8ad25949d2cfd11c3
ajeet <ajeetkumar23898@gmail.com>
2022-03-13 13:48:07 +0530

    ok

---
+++ creed/src/credentialGenerator/CredentialService.java
@@ -0,0 +1,61 @@
+package credentialGenerator;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class CredentialService {
+	String email, password;
+	static String company = "abc";;
+	void generateEmailAddress(String fullName, String dep){
+		email =fullName + "@" + dep.replaceAll("\\s", "") +"."+company+".com";
+		//return email;
+	}
+	Random ran = new Random();
+	String generate(String str, int size) {
+		String result;
+		int ran1 = 1+ran.nextInt(size);
+		int ran2 = 1+ran.nextInt(size); 
+		if(ran1 > ran2) {
+			result = str.substring(ran2,ran1);
+		}
+		else {
+			result = str.substring(ran1, ran2);
+		}
+		return result;
+	}
+	String generatePassword(){
+		String upper= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String lower = "abcdefghijklmnopqrstuvwxyz";
+		String num  = "0123456789";
+		String charc = "@!#$_-&";
+		int charcSize = charc.length();
+		
+		String password = generate(upper,26)+generate(lower,26)+generate(num,10)+generate(charc,charcSize);
+		
+// shuffling the password
+		char [] charArray = password.toCharArray();
+		List<Character> li = new ArrayList<Character>();
+		for(int i=0; i<charArray.length; i++) {
+			li.add(charArray[i]);
+		}
+		Collections.shuffle(li);
+		String finalPassword = "";
+		for(int i=0; i<li.size(); i++) {
+			finalPassword +=li.get(i);
+		}
+		return finalPassword;
+		
+	}
+	void  showCredential(String fName, String lName, String dep){
+		String fullName = fName + lName;
+		generateEmailAddress(fullName,dep);
+		System.out.println("Dear " + fName + " Your generated credential are as follows" );
+		System.out.println("Email ----> "+email);
+		System.out.println("Password ----> " +generatePassword());
+		
+		
+		//System.out.println(email);
+	}
+}
